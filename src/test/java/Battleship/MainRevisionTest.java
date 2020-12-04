f9c1207c2e0efaef8ad023f9067203c08540715d
WASHINGTON\washington <washington.p.o@gmail.com>
2020-12-04 12:44:49 +0100

    Nuevos archivos de Test

---
+++ src/test/java/Battleship/MainRevisionTest.java
@@ -0,0 +1,14 @@
+package Battleship;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MainRevisionTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/Battleship/PlayerRevisionTest.java
@@ -0,0 +1,14 @@
+package Battleship;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PlayerRevisionTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
